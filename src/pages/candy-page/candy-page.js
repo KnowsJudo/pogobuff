41f8d3e00424fbc2c0438c3a5fd673a69b74b3d3
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2022-11-02 17:02:26 +1100

    candy

--- src/pages/candy-page/candy-page.js
+++ src/pages/candy-page/candy-page.js
@@ -1,19 +1,37 @@
 import { NavBar } from "../../components/nav-bar/nav-bar";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../context";
 import rareC from "../../img/rare.webp";
 import xLC from "../../img/xl-candy.webp";
+import { Button, Input, List, Tooltip } from "@material-ui/core";
 import "./candy-page.css";
-import { Input } from "@material-ui/core";
 
 export const CandyPage = () => {
   const { userData, setUserData } = useContext(UserContext);
+  const [candidates, setCandidates] = useState([""]);
+  const [newCandidate, setNewCandidate] = useState("");
 
   const handleCandy = (event, type) => {
     event.preventDefault();
     setUserData((prev) => {
-      return { ...prev, [type]: event.target.value };
+      return {
+        ...prev,
+        [type]: event.target.value > 0 ? event.target.value : 0,
+      };
     });
+  };
+
+  const handleCandidate = (event) => {
+    event.preventDefault();
+    setNewCandidate(event.target.value);
+  };
+
+  const addNewCandidate = () => {
+    if (!newCandidate) {
+      return;
+    }
+    setCandidates((prev) => [...prev, newCandidate]);
+    setNewCandidate("");
   };
 
   return (
@@ -40,7 +58,20 @@ export const CandyPage = () => {
         </h5>
       </div>
       <div className="candy-pokes">
-        <h5></h5>
+        <span>
+          <h5>Candy Priorities:</h5>
+          <Input
+            type="string"
+            onChange={(e) => handleCandidate(e)}
+            value={newCandidate}
+          ></Input>
+          <Tooltip title="Add to List">
+            <Button onClick={() => addNewCandidate()}>{"->"}</Button>
+          </Tooltip>
+        </span>
+        {candidates.map((next, i) => {
+          return <List key={i}>{next}</List>;
+        })}
       </div>
     </section>
   );
