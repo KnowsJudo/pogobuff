f05634af49277fe0219075d43b510c1a2c38b78c
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2023-02-26 21:20:59 +1100

    home grid

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,23 +1,50 @@
 import React from "react";
-import { Grid } from "@mui/material";
-import { GridItem } from "../../components/grid-item/grid-item";
+import { Button, Grid } from "@mui/material";
 import heading from "../../img/pogo.png";
+import HorizontalRuleRoundedIcon from "@mui/icons-material/HorizontalRuleRounded";
 import "./home-page.css";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
   return (
     <section className="home-container">
       <img src={heading} alt="pogobuff" className="pogobuff-heading" />
-      <Grid
-        container
-        columnSpacing={10}
-        rowSpacing={2}
-        style={{ marginLeft: "25%" }}
-      >
-        <GridItem link="sets" title="GBL Sets Tracker" />
-        <GridItem link="candy" title="Rare Candy Candidates" />
-        <GridItem link="teams" title="GBL Teams" />
-        <GridItem link="tms" title="Elite Tms Candidates" />
+      <HorizontalRuleRoundedIcon
+        preserveAspectRatio="none"
+        style={{
+          height: "30px",
+          width: "100%",
+        }}
+      />
+      <Grid container spacing={5} style={{ margin: "3% 0 0 33%" }}>
+        <Grid item>
+          <Link to="/sets" style={{ textDecoration: "none" }}>
+            <Button variant="contained" style={{ backgroundColor: "black" }}>
+              GBL Sets Tracker
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link to="/candy" style={{ textDecoration: "none" }}>
+            <Button variant="contained" style={{ backgroundColor: "black" }}>
+              Rare Candy Candidates
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link to="/teams" style={{ textDecoration: "none" }}>
+            <Button variant="contained" style={{ backgroundColor: "black" }}>
+              GBL Teams
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link to="/elite" style={{ textDecoration: "none" }}>
+            <Button variant="contained" style={{ backgroundColor: "black" }}>
+              Elite Tms Candidates
+            </Button>
+          </Link>
+        </Grid>
       </Grid>
     </section>
   );
