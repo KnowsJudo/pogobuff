41c740baaab892e5129f5636a44722108a1c0043
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2023-03-15 16:23:13 +1100

    candy name

--- src/pages/candy-page/candy-page.tsx
+++ src/pages/candy-page/candy-page.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import axios from "axios";
+import { apiURL } from "../../helpers/api-url";
 import { NavBar } from "../../components/nav-bar/nav-bar";
 import { useContext, useState } from "react";
 import { UserContext } from "../../context";
 import { Button, Input, List, ListItem, Tooltip } from "@material-ui/core";
 import { BackButton } from "../../components/back-button/back-button";
 import { IUserState } from "../../types/elo";
+import DoneIcon from "@mui/icons-material/Done";
+import EditIcon from "@mui/icons-material/Edit";
 import rareC from "../../img/rare.webp";
 import xLC from "../../img/xl-candy.webp";
 import "./candy-page.css";
 
 export const CandyPage = () => {
   const { userData, setUserData } = useContext(UserContext);
+  const [name, setName] = useState<string>("");
+  const [nameEntered, setNameEntered] = useState(false);
   const [candidates, setCandidates] = useState([""]);
   const [newCandidate, setNewCandidate] = useState("");
 
@@ -42,29 +48,76 @@ export const CandyPage = () => {
     setNewCandidate("");
   };
 
+  const submitName = async () => {
+    try {
+      await axios.post(`${apiURL}/api/candy`, { playername: name });
+      const data = await axios.get(`${apiURL}/api/candy`);
+      console.log(data);
+      setNameEntered(true);
+      setUserData((prev: IUserState) => {
+        return {
+          ...prev,
+          elo: {
+            ...prev.elo,
+            playername: name,
+          },
+          sets: [...prev.sets],
+        };
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <section className="candy-page">
       <NavBar />
       <BackButton />
       <div className="current-candy">
-        <h5>
-          Current: <img height={100} width={100} src={rareC} alt="rare candy" />
-          &nbsp;x&nbsp;
-          <Input
-            type="number"
-            onChange={(e) => handleCandy(e, "candy")}
-            value={userData.candy}
-          />
-        </h5>
-        <h5>
-          Current XL: <img height={70} width={70} src={xLC} alt="XL candy" />
-          &nbsp;x&nbsp;
-          <Input
-            type="number"
-            onChange={(e) => handleCandy(e, "candyXL")}
-            value={userData.candyXL}
-          />
-        </h5>
+        <h6>Playername:</h6>
+        {!nameEntered ? (
+          <span className="enter-elo">
+            <Input
+              value={name}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setName(e.target.value)
+              }
+            />
+            <Button onClick={() => submitName()}>
+              <DoneIcon />
+            </Button>
+          </span>
+        ) : (
+          <span className="edit-elo">
+            <h6>{userData.elo.playername}</h6>
+            <Tooltip title="Edit">
+              <EditIcon
+                onClick={() => setNameEntered(false)}
+                sx={{
+                  fontSize: 14,
+                  marginLeft: "25%",
+                  "&:hover": { cursor: "pointer" },
+                }}
+              />
+            </Tooltip>
+          </span>
+        )}
+        <h5>Current: </h5>
+        <img height={70} width={70} src={rareC} alt="rare candy" />
+        &nbsp;x&nbsp;
+        <Input
+          type="number"
+          onChange={(e) => handleCandy(e, "candy")}
+          value={userData.candy}
+        />
+        <h5>Current XL:</h5>{" "}
+        <img height={45} width={45} src={xLC} alt="XL candy" />
+        &nbsp;x&nbsp;
+        <Input
+          type="number"
+          onChange={(e) => handleCandy(e, "candyXL")}
+          value={userData.candyXL}
+        />
       </div>
       <div className="candy-pokes">
         <span className="candly-list">
