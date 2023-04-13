899e426dc41e63efebcdf18d943d8ea18c8714f1
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2023-04-13 22:57:27 +1000

    fixxes

--- src/pages/teams-page/teams-page.tsx
+++ src/pages/teams-page/teams-page.tsx
@@ -34,14 +34,16 @@ const initialTeams = {
   closer: "",
 };
 
+const initialEdit = {
+  lead: false,
+  switch: false,
+  closer: false,
+};
+
 export const TeamsPage: React.FC = () => {
   const [league, setLeague] = useState<string>("");
   const [nextTeam, setNextTeam] = useState<ITeam>(initialTeams);
-  const [editNext, setEditNext] = useState<INextTeam>({
-    lead: false,
-    switch: false,
-    closer: false,
-  });
+  const [editNext, setEditNext] = useState<INextTeam>(initialEdit);
   const [teams, setTeams] = useState<ITeam[]>([
     { lead: "Araquanid", switch: "Cradily", closer: "Ferrothorn" },
   ]);
@@ -60,6 +62,7 @@ export const TeamsPage: React.FC = () => {
       return [...prev, nextTeam];
     });
     setNextTeam(initialTeams);
+    setEditNext(initialEdit);
   };
 
   return (
@@ -80,91 +83,93 @@ export const TeamsPage: React.FC = () => {
           </Select>
         </FormControl>
       </div>
-      <div className="teams-list">
-        <span className="teams-head">
-          {league}
-          <Button
-            onClick={() => addTeam()}
-            color="secondary"
-            size="small"
-            style={{ marginLeft: "auto" }}
-            variant="outlined"
-          >
-            <AddIcon />
-            New
-          </Button>
-        </span>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <b>Lead</b>
-              </TableCell>
-              <TableCell>
-                <b>Switch</b>
-              </TableCell>
-              <TableCell>
-                <b>Closer</b>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-
-          {teams.map((next) => {
-            return (
+      {league && (
+        <div className="teams-list">
+          <span className="teams-head">
+            {league}
+            <Button
+              onClick={() => addTeam()}
+              color="secondary"
+              size="small"
+              style={{ marginLeft: "auto" }}
+              variant="outlined"
+            >
+              <AddIcon />
+              New
+            </Button>
+          </span>
+          <Table>
+            <TableHead>
               <TableRow>
-                <TableCell>{next.lead}</TableCell>
-                <TableCell>{next.switch}</TableCell>
-                <TableCell>{next.closer}</TableCell>
+                <TableCell>
+                  <b>Lead</b>
+                </TableCell>
+                <TableCell>
+                  <b>Switch</b>
+                </TableCell>
+                <TableCell>
+                  <b>Closer</b>
+                </TableCell>
               </TableRow>
-            );
-          })}
+            </TableHead>
 
-          <TableRow>
-            {editNext.lead && (
-              <TableCell>
-                <Input
-                  value={nextTeam.lead}
-                  onChange={(e) =>
-                    setNextTeam((prev) => {
-                      return { ...prev, lead: e.target.value };
-                    })
-                  }
-                />
-              </TableCell>
-            )}
-            {editNext.switch && (
-              <TableCell>
-                <Input
-                  value={nextTeam.switch}
-                  onChange={(e) =>
-                    setNextTeam((prev) => {
-                      return { ...prev, switch: e.target.value };
-                    })
-                  }
-                />
-              </TableCell>
-            )}
-            {editNext.closer && (
-              <TableCell>
-                <Input
-                  value={nextTeam.closer}
-                  onChange={(e) =>
-                    setNextTeam((prev) => {
-                      return { ...prev, closer: e.target.value };
-                    })
-                  }
-                />
-                <Button
-                  style={{ color: "black" }}
-                  onClick={() => addTeamToList()}
-                >
-                  <DoneIcon />
-                </Button>
-              </TableCell>
-            )}
-          </TableRow>
-        </Table>
-      </div>
+            {teams.map((next) => {
+              return (
+                <TableRow>
+                  <TableCell>{next.lead}</TableCell>
+                  <TableCell>{next.switch}</TableCell>
+                  <TableCell>{next.closer}</TableCell>
+                </TableRow>
+              );
+            })}
+
+            <TableRow>
+              {editNext.lead && (
+                <TableCell>
+                  <Input
+                    value={nextTeam.lead}
+                    onChange={(e) =>
+                      setNextTeam((prev) => {
+                        return { ...prev, lead: e.target.value };
+                      })
+                    }
+                  />
+                </TableCell>
+              )}
+              {editNext.switch && (
+                <TableCell>
+                  <Input
+                    value={nextTeam.switch}
+                    onChange={(e) =>
+                      setNextTeam((prev) => {
+                        return { ...prev, switch: e.target.value };
+                      })
+                    }
+                  />
+                </TableCell>
+              )}
+              {editNext.closer && (
+                <TableCell>
+                  <Input
+                    value={nextTeam.closer}
+                    onChange={(e) =>
+                      setNextTeam((prev) => {
+                        return { ...prev, closer: e.target.value };
+                      })
+                    }
+                  />
+                  <Button
+                    style={{ color: "black" }}
+                    onClick={() => addTeamToList()}
+                  >
+                    <DoneIcon />
+                  </Button>
+                </TableCell>
+              )}
+            </TableRow>
+          </Table>
+        </div>
+      )}
     </section>
   );
 };
