a5a0d3472fac12743464236f1cfdc1ab84d23c2b
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2023-04-24 21:56:51 +1000

    api test

--- src/helpers/api-url.ts
+++ src/helpers/api-url.ts
@@ -1,1 +1,3 @@
 export const apiURL = "http://localhost:3001";
+
+export const pokeApi = "https://pokeapi.co/api/v2/";
--- src/pages/rewards-page/rewards-page.tsx
+++ src/pages/rewards-page/rewards-page.tsx
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { BackButton } from "../../components/back-button/back-button";
 import { NavBar } from "../../components/nav-bar/nav-bar";
-import { Input } from "@mui/material";
+import { Button, Input } from "@mui/material";
+import { pokeApi } from "../../helpers/api-url";
 import "./rewards-page.css";
 
+interface IPoke {
+  name: string;
+  url: string;
+}
+
 export const RewardsPage: React.FC = () => {
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const getPokemon: () => void = async () => {
+    try {
+      const data = await axios.get(`${pokeApi}pokemon?limit=2000&offset=0`);
+      const results = data.data.results;
+      console.log(results);
+      console.log("search", searchInput);
+      const match = results.filter((match: IPoke) =>
+        match.name.toLowerCase().includes(searchInput.toLowerCase())
+      );
+      console.log("match", match);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <section className="rewards-page">
       <NavBar />
       <BackButton />
       <div className="rewards-input">
-        <Input placeholder="Encounter"></Input>
-        <Input type="number" placeholder="Seen"></Input>
+        <span>
+          <Input
+            placeholder="Encounter"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <Button onClick={() => getPokemon()}>Search</Button>
+        </span>
+        <Input type="number" placeholder="Seen" />
       </div>
     </section>
   );
