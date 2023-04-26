531c03b06b928c311976ee9aa3e3fbcf8f0ff2cd
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2023-04-26 13:28:32 +1000

    add debouncer

--- src/pages/rewards-page/rewards-page.tsx
+++ src/pages/rewards-page/rewards-page.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { BackButton } from "../../components/back-button/back-button";
 import { NavBar } from "../../components/nav-bar/nav-bar";
 import { Button, Input } from "@mui/material";
 import { pokeApi } from "../../helpers/api-url";
+import { debounce } from "../../helpers/debounce";
 import "./rewards-page.css";
 
 interface IPoke {
@@ -14,20 +15,31 @@ interface IPoke {
 export const RewardsPage: React.FC = () => {
   const [searchInput, setSearchInput] = useState<string>("");
 
-  const getPokemon: () => void = async () => {
-    try {
-      const data = await axios.get(`${pokeApi}pokemon?limit=2000&offset=0`);
-      const results = data.data.results;
-      console.log(results);
-      console.log("search", searchInput);
-      const match = results.filter((match: IPoke) =>
-        match.name.toLowerCase().includes(searchInput.toLowerCase())
-      );
-      console.log("match", match);
-    } catch (error) {
-      console.error(error);
+  const getPokemon: () => void = useMemo(
+    () =>
+      debounce(async () => {
+        try {
+          const data = await axios.get(`${pokeApi}pokemon?limit=2000&offset=0`);
+          const results = data.data.results;
+          console.log(results);
+          console.log("search", searchInput);
+          const match = results.filter((match: IPoke) =>
+            match.name.toLowerCase().includes(searchInput.toLowerCase())
+          );
+          console.log("match", match);
+        } catch (error) {
+          console.error(error);
+        }
+      }, 500),
+    [searchInput]
+  );
+
+  useEffect(() => {
+    if (searchInput.length < 3 || searchInput.length > 10) {
+      return;
     }
-  };
+    getPokemon();
+  }, [searchInput, getPokemon]);
 
   return (
     <section className="rewards-page">
